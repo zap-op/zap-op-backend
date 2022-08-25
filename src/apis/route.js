791f1cd1336391ca6e1e350f4a292b4c596ab63b
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-08-25 13:19:25 +0700

    Fix import router syntax

--- src/apis/route.js
+++ src/apis/route.js
@@ -1,5 +1,5 @@
-import { router as loginRouter } from './routers/login.router/login.router.js';
-import { router as spiderRouter } from './routers/spider.router/spider.router.js';
+import { router as loginRouter } from './routers/login-router/login.router.js';
+import { router as spiderRouter } from './routers/spider-router/spider.router.js';
 
 export function initRoutes(app) {
     app.use('/login', loginRouter);
