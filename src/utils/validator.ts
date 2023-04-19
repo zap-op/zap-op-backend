21824d525d2f6acaf62d4d217620e292cfa24e11
Huy Pham <pham.giahuy.9041@gmail.com>
2023-04-19 18:23:26 +0700

    ZO-177 Update validator.js prettier format.

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,31 +1,30 @@
 import validator from "validator";
 import urlExists from "url-exists-nodejs";
-import {OAuth2Client} from "google-auth-library";
+import { OAuth2Client } from "google-auth-library";
 
 export async function isValidURL(urlString: string) {
-    return !urlString.includes("localhost") &&
-        !urlString.includes("127.0.0.1") &&
-        validator.isURL(urlString, {
-            protocols: ["http", "https"],
-            require_protocol: true,
-            allow_underscores: true,
-        }) &&
-        await urlExists(urlString);
+	return (
+		!urlString.includes("localhost") &&
+		!urlString.includes("127.0.0.1") &&
+		validator.isURL(urlString, {
+			protocols: ["http", "https"],
+			require_protocol: true,
+			allow_underscores: true,
+		}) &&
+		(await urlExists(urlString))
+	);
 }
 
-if (!process.env.GOOGLE_CLIENT_ID)
-    throw "GOOGLE_CLIENT_ID not found";
+if (!process.env.GOOGLE_CLIENT_ID) throw "GOOGLE_CLIENT_ID not found";
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export async function isValidGoogleIDToken(token: string) {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.GOOGLE_CLIENT_ID
-    });
-    const payload = ticket.getPayload();
-    if (!payload)
-        throw "No payload";
-
-    return payload;
+	const ticket = await client.verifyIdToken({
+		idToken: token,
+		audience: process.env.GOOGLE_CLIENT_ID,
+	});
+	const payload = ticket.getPayload();
+	if (!payload) throw "No payload";
+	return payload;
 }
