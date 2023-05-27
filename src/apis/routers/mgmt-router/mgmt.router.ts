d854706e8198f1fa83c7bf89694735a084c19dad
Huy Pham <pham.giahuy.9041@gmail.com>
2023-05-27 09:17:12 +0700

    ZO-196 Update prettier format.

--- src/apis/routers/mgmt-router/mgmt.router.ts
+++ src/apis/routers/mgmt-router/mgmt.router.ts
@@ -8,81 +8,89 @@ import { MGMT_STATUS } from "../../../utils/types";
 import { mainProc } from "../../../services/logging.service";
 
 export function getMgmtRouter(): Router {
-    const mgmtRouter = express.Router();
-    const validator = new Validator({});
+	const mgmtRouter = express.Router();
+	const validator = new Validator({});
 
-    const postTargetSchema: JSONSchema7 = {
-        type: "object",
-        properties: {
-            name: {
-                type: "string",
-            },
-            target: {
-                type: "string",
-            },
-            tag: {
-                type: "array",
-                items: {
-                    "type": "string"
-                }
-            }
-        },
-        required: ["name", "target"],
-    };
+	const postTargetSchema: JSONSchema7 = {
+		type: "object",
+		properties: {
+			name: {
+				type: "string",
+			},
+			target: {
+				type: "string",
+			},
+			tag: {
+				type: "array",
+				items: {
+					type: "string",
+				},
+			},
+		},
+		required: ["name", "target"],
+	};
 
-    mgmtRouter.get("/targets", async (req: JWTRequest, res) => {
-        const targets = await targetModel.find({ "userId": req.accessToken!.userId });
-        res.status(200).json(targets);
-    });
+	mgmtRouter.get("/targets", async (req: JWTRequest, res) => {
+		const targets = await targetModel.find({
+			userId: req.accessToken!.userId,
+		});
+		res.status(200).json(targets);
+	});
 
-    mgmtRouter.post("/target", validator.validate({ body: postTargetSchema }),
-        async (req: JWTRequest, res) => {
-            const body = req.body;
+	mgmtRouter.post("/target", validator.validate({ body: postTargetSchema }), async (req: JWTRequest, res) => {
+		const body = req.body;
 
-            if (!(await isValidURL(body.target)))
-                return res.status(400).send(MGMT_STATUS.TARGET_INVAVLID_URL);
+		if (!(await isValidURL(body.target))) {
+			return res.status(400).send(MGMT_STATUS.TARGET_INVAVLID_URL);
+		}
 
-            try {
-                if (await targetModel.findOne({
-                    userId: req.accessToken!.userId,
-                    name: body.name
-                }))
-                    return res.status(400).send(MGMT_STATUS.TARGET_NAME_DUPLICATE);
+		try {
+			if (
+				await targetModel.findOne({
+					userId: req.accessToken!.userId,
+					name: body.name,
+				})
+			) {
+				return res.status(400).send(MGMT_STATUS.TARGET_NAME_DUPLICATE);
+			}
 
-                const newTarget = new targetModel({
-                    userId: req.accessToken!.userId,
-                    name: body.name,
-                    target: body.target,
-                    tag: body.tag ?? []
-                });
-                await newTarget.save();
+			const newTarget = new targetModel({
+				userId: req.accessToken!.userId,
+				name: body.name,
+				target: body.target,
+				tag: body.tag ?? [],
+			});
+			await newTarget.save();
 
-                return res.status(201).send(MGMT_STATUS.TARGET_ADDED);
-            } catch (error) {
-                mainProc.error(`Error while adding new target: ${error}`);
-                res.status(500).send(MGMT_STATUS.TARGET_ADD_FAILED);
-            }
-        });
+			return res.status(201).send(MGMT_STATUS.TARGET_ADDED);
+		} catch (error) {
+			mainProc.error(`Error while adding new target: ${error}`);
+			res.status(500).send(MGMT_STATUS.TARGET_ADD_FAILED);
+		}
+	});
 
-    mgmtRouter.delete("/target", async (req: JWTRequest, res) => {
-        if (!req.query.id)
-            return res.status(400).send(MGMT_STATUS.TARGET_INVALID_ID);
+	mgmtRouter.delete("/target", async (req: JWTRequest, res) => {
+		if (!req.query.id) {
+			return res.status(400).send(MGMT_STATUS.TARGET_INVALID_ID);
+		}
 
-        try {
-            const target = await targetModel.findById(req.query.id);
-            if (!target || target.userId.toString() !== req.accessToken!.userId)
-                return res.status(400).send(MGMT_STATUS.TARGET_FIND_FAILED);
+		try {
+			const target = await targetModel.findById(req.query.id);
+			if (!target || target.userId.toString() !== req.accessToken!.userId) {
+				return res.status(400).send(MGMT_STATUS.TARGET_FIND_FAILED);
+			}
 
-            const trashedTarget = new targetTrashModel(target.toObject());
+			const trashedTarget = new targetTrashModel(target.toObject());
 
-            await trashedTarget.save();
-            await target.deleteOne();
+			await trashedTarget.save();
+			await target.deleteOne();
 
-            return res.status(200).send(MGMT_STATUS.TARGET_MOVED_TO_TRASH);
-        } catch (error) {
-            mainProc.error(`Error while deleting target: ${error}`);
-            res.status(500).send(MGMT_STATUS.TARGET_DELETE_FAILED);
-        }});
+			return res.status(200).send(MGMT_STATUS.TARGET_MOVED_TO_TRASH);
+		} catch (error) {
+			mainProc.error(`Error while deleting target: ${error}`);
+			res.status(500).send(MGMT_STATUS.TARGET_DELETE_FAILED);
+		}
+	});
 
-    return mgmtRouter;
+	return mgmtRouter;
 }
